fd1c8d135ab70b3d629e9ba5726162f75fe1e85d
rom4k-vzlom4k <dangerkrevetka@gmail.com>
2025-07-16 01:31:52 +0300

    It is done

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,18 +1,39 @@
 package storage
 
-import "rom4k-vzlom4k/storage/internal/file"
+import (
+	"fmt"
+	"rom4k-vzlom4k/storage/internal/file"
 
-type Storage struct{}
+	"github.com/google/uuid"
+)
+
+type Storage struct {
+	Files map[uuid.UUID]*file.File
+}
 
 func NewStorage() *Storage {
-	return &Storage{}
+	return &Storage{
+		Files: make(map[uuid.UUID]*file.File),
+	}
 }
 
 func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
-	return file.NewFile(filename, blob)
-	// if err != nil {
-	// 	return nil, err // При ошибке, лучше постоянно возвращатиь nil
-	// }
 
-	// return newFile
+	newFile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err // при ошибке, лучше постоянно возвращатиь nil
+	}
+
+	s.Files[newFile.ID] = newFile
+
+	return newFile, nil
+}
+
+func (s *Storage) GetById(fileID uuid.UUID) (*file.File, error) {
+	foundFile, ok := s.Files[fileID]
+	if !ok {
+		return nil, fmt.Errorf("file %v not found", fileID)
+	}
+
+	return foundFile, nil
 }
