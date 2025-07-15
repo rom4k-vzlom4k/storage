de073b3cb50bc5035b2d08aae408720321f1c65b
rom4k-vzlom4k <dangerkrevetka@gmail.com>
2025-07-16 01:07:57 +0300

    Add simple upload method

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,6 +1,21 @@
 package file
 
+import "github.com/google/uuid"
+
 type File struct {
+	ID   uuid.UUID
 	Name string
 	Data []byte
+}
+
+func NewFile(filename string, blob []byte) (*File, error) {
+	fileId, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	return &File{
+		ID:   fileId,
+		Name: filename,
+		Data: blob,
+	}, err
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,18 @@
 package storage
 
+import "rom4k-vzlom4k/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+	// if err != nil {
+	// 	return nil, err // При ошибке, лучше постоянно возвращатиь nil
+	// }
+
+	// return newFile
 }
